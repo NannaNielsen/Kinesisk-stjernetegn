c46040be4621e4d89677531c46eb403dfa4b11bb
Nanna <cph-nn238@cphbusiness.dk>
2025-05-27 13:32:28 +0200

    find stjenetegn fiks

--- script.js
+++ script.js
@@ -12,6 +12,22 @@ function scriptButton() {
     if (birthdayM < 10) birthdayM = "0" + birthdayM;
     if (birthdayD < 10) birthdayD = "0" + birthdayD;
     var birthday = birthdayY + birthdayM + birthdayD;
+    
+// Mapping af billeder til hvert stjernetegn
+    const signImages = {
+        rotten: "img/Mus-m-baggrund.png",
+        oksen: "img/Ko-m-baggrund.png",
+        tigeren: "img/Tiger-m-baggrund.png",
+        kaninen: "img/Kanin-m-baggrund.png",
+        dragen: "img/Drage-m-baggrund.png",
+        slangen: "img/Slange-m-baggrund.png",
+        hesten: "img/Hest-m-baggrund.png",
+        geden: "img/Ged-m-baggrund.png",
+        aben: "img/Abe-m-baggrund.png",
+        hanen: "img/Hane-m-baggrund.png",
+        hunden: "img/Hund-m-baggrund.png",
+        grisen: "img/Gris-m-baggrund.png"
+    };
 
     // Forkortet signs array til eksempel — indsæt dit fulde array her
     const signs = [
